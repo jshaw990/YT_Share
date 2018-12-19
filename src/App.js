b3b4ed2a7ddc1ec0ba4a8e5fcee5c1ad6515749b
Jayden Shaw <jayden.shaw@gmail.com>
2018-12-19 12:18:54 -0700

    Removed all Pusher files. Updated Node Modules to reflect change.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,4 @@
 import React, { Component } from 'react';
-import axios from 'axios';
-import Pusher from 'pusher-js';
-import ChatList from './components/ChatList';
-import ChatBox from './components/ChatBox';
 import TopNav from './components/TopNav';
 import SearchBar from './components/SearchBar.jsx';
 import YTSearch from 'youtube-api-search';
@@ -18,9 +14,6 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      text: '',
-      username: '',
-      chats: [],
       videos: [],
       selectedVideo: null
     };
@@ -38,32 +31,6 @@ class App extends Component {
     });
   }
 
-  componentDidMount() {
-    const username = window.prompt('Username: ', 'Anonymous');
-    this.setState({ username });
-    const pusher = new Pusher('process.env.APP_KEY', {
-      cluster: 'process.env.APP_CLUSTER',
-      encrypted: true
-    });
-    const channel = pusher.subscribe('chat');
-    channel.bind('message', data => {
-      this.setState({ chats: [...this.state.chats, data], test: '' });
-    });
-    this.handleTextChange = this.handleTextChange.bind(this);
-  }
-
-  handleTextChange(e) {
-    if (e.keyCode === 13) {
-      const payload = {
-        username: this.state.username,
-        message: this.state.text
-      };
-      axios.post('http://localhost:5000/message', payload);
-    } else {
-      this.setState({ text: e.target.value });
-    }
-  }
-
   render() {
     return (
       <div className="App">
@@ -74,14 +41,6 @@ class App extends Component {
         <VideoPlayer video={this.state.selectedVideo}/>
         <VideoList onVideoSelect={userSelected => this.setState({selectedVideo: userSelected})}
                    videos={this.state.videos} />
-        <section>
-          <ChatList chats={this.state.chats} />
-          <ChatBox
-            text={this.state.text}
-            username={this.state.username}
-            handleTextChange={this.handleTextChange}
-          />
-        </section>
       </div>
     );
   }
